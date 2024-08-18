138e6dad72f4acd036993111352ab7cc5148bf3b
Omar Mohamed <70483549+omarragab1948@users.noreply.github.com>
2024-08-18 16:00:47 +0300

    finish fist slider

--- src/sections/home/hero/news/index.tsx
+++ src/sections/home/hero/news/index.tsx
@@ -1,31 +1,84 @@
 "use client";
 
 import styles from "./styles.module.css";
-import Slider from "@/components/slider";
+import { Box, Button, Grid, Rating, Typography } from "@mui/material";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import Slider from "../../slider";
 
-export default function HomeNews() {
-  const [slides, setSlides] = useState<any>();
+export default function HomeNews({slidess}) {
+  // const [slides, setSlides] = useState<any>();
 
-  useEffect(() => {
-    setTimeout(() => {
-      setSlides(
-        Array.from({ length: 18 })?.map((_, index) => (
-          <div
-            className={styles.card}
-            onClick={() => console.log(index)}
-            role="button"
-            key={index}
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setSlides(
+  //       Array.from({ length: 18 })?.map((_, index) => (
+  //         <div
+  //           className={styles.card}
+  //           onClick={() => console.log(index)}
+  //           role="button"
+  //           key={index}
+  //         >
+  //           <p className={styles.title}>Jun 14, 2024</p>
+  //           <h4 className={styles.content}>
+  //             Andersen Sponsors Digital Uzbekistan 2024 Forum
+  //           </h4>
+  //         </div>
+  //       ))
+  //     );
+  //   }, 1000);
+  // }, []);
+  const slides = slidess?.map((slide, index)=>(
+    <Grid
+    key={index}
+            container
+            spacing={2}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: slide.bgColor,
+              color: "#fff",
+              padding: 2,
+              width: "100%",
+              borderRadius: "10px",
+
+            }}
           >
-            <p className={styles.title}>Jun 14, 2024</p>
-            <h4 className={styles.content}>
-              Andersen Sponsors Digital Uzbekistan 2024 Forum
-            </h4>
-          </div>
-        ))
-      );
-    }, 1000);
-  }, []);
+            <Grid item xs={12} lg={5} sx={{ textAlign: "left", padding: 2, height: "350px" }}>
+              <Typography variant="h4" component="h2" gutterBottom>
+                {slide.title}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                {slide.description}
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                Available on --
+              </Typography>
+              <Box sx={{ display: "flex", alignItems: "center", marginTop: 1 }}>
+                <Rating value={slide.rating} readOnly precision={0.5} />
+                <Typography variant="body2" sx={{ marginLeft: 1 }}>
+                  {slide.rating}/5 Ratings
+                </Typography>
+              </Box>
+              <Typography variant="body2" sx={{ marginTop: 1 }}>
+                {slide.employeeUsage} Employee Usage
+              </Typography>
+              <Button variant="contained" color="success" sx={{ marginTop: 2 }}>
+                {slide.buttonLabel}
+              </Button>
+            </Grid>
+            <Grid item xs={12} lg={4} sx={{ marginLeft: 4 }}>
+              <Image
+                src={slide.image}
+                alt={slide.title}
+                style={{ height: "50%" , width: "80%"}}
+                width={1000}
+                height={1000}
+              />
+            </Grid>
+          </Grid>
+  ))
 
   if (slides) return <Slider slides={slides} />;
 
